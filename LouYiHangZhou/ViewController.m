a54e2f4e51d1756d1afa0187d219817da452b959
Lenshen <779822644@qq.com>
2016-04-07 19:22:56 +0800

    Revert "test"
    This reverts commit 472e3c16a1bba6b725372af363a841e34d5bc5cf.

---
+++ LouYiHangZhou/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  LouYiHangZhou
+//
+//  Created by 远深 on 16/4/7.
+//  Copyright © 2016年 Luo Yi TECHNOLOGY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
