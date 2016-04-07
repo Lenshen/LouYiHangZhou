4499d1de15a939d49f39222156c24496454a4573
Lenshen <779822644@qq.com>
2016-04-07 19:20:59 +0800

    test

---
+++ LouYiHangZhou/UISearchBar+dismissBackground.h
@@ -0,0 +1,13 @@
+//
+//  UISearchBar+dismissBackground.h
+//  LouYiHangZhou
+//
+//  Created by 远深 on 16/4/7.
+//  Copyright © 2016年 Luo Yi TECHNOLOGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISearchBar (dismissBackground)
+
+@end
---
+++ LouYiHangZhou/UISearchBar+dismissBackground.m
@@ -0,0 +1,13 @@
+//
+//  UISearchBar+dismissBackground.m
+//  LouYiHangZhou
+//
+//  Created by 远深 on 16/4/7.
+//  Copyright © 2016年 Luo Yi TECHNOLOGY. All rights reserved.
+//
+
+#import "UISearchBar+dismissBackground.h"
+
+@implementation UISearchBar (dismissBackground)
+
+@end
